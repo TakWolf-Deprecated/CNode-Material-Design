931e8e38512cb7c19dd9ebcaa5cccf9a64c0bd8c
TakWolf <takwolf@foxmail.com>
2015-08-02 01:31:29 +0800

    代码整理

--- app/src/main/java/org/cnodejs/android/md/model/entity/TabType.java
+++ app/src/main/java/org/cnodejs/android/md/model/entity/TabType.java
@@ -4,49 +4,24 @@ import org.cnodejs.android.md.R;
 
 public enum TabType {
 
-    all(R.string.tab_all, R.id.main_left_btn_all),
+    all(R.string.tab_all),
 
-    good(R.string.tab_good, R.id.main_left_btn_good),
+    good(R.string.tab_good),
 
-    share(R.string.tab_share, R.id.main_left_btn_share),
+    share(R.string.tab_share),
 
-    ask(R.string.tab_ask, R.id.main_left_btn_ask),
+    ask(R.string.tab_ask),
 
-    job(R.string.tab_job, R.id.main_left_btn_job);
+    job(R.string.tab_job);
 
     private int nameId;
 
-    private int actionId;
-
-    TabType(int nameId, int actionId) {
+    TabType(int nameId) {
         this.nameId = nameId;
-        this.actionId = actionId;
     }
 
     public int getNameId() {
         return nameId;
-    }
-
-    public int getActionId() {
-        return actionId;
-    }
-
-    public static TabType actionIdOf(int actionId) {
-        switch (actionId) {
-            case R.id.main_left_btn_all:
-                return all;
-            case R.id.main_left_btn_good:
-                return good;
-            case R.id.main_left_btn_share:
-                return share;
-            case R.id.main_left_btn_ask:
-                return ask;
-            case R.id.main_left_btn_job:
-                return job;
-            default:
-                return all;
-        }
-
     }
 
 }
