4972241bb5c594b53fb51068636fb3a64a3d9940
TakWolf <takwolf@foxmail.com>
2015-08-01 20:02:40 +0800

    基础架构

--- app/src/main/java/org/cnodejs/android/md/activity/LoginActivity.java
+++ app/src/main/java/org/cnodejs/android/md/activity/LoginActivity.java
@@ -1,14 +1,16 @@
-package org.cnodejs.android.md;
+package org.cnodejs.android.md.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import org.cnodejs.android.md.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
 }
