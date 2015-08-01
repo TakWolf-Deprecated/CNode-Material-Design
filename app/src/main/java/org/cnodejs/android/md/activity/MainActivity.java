8bbcf891f0118211c0b59e30ffecc95686f95ea2
TakWolf <takwolf@foxmail.com>
2015-08-01 23:33:15 +0800

    主页抽屉菜单

--- app/src/main/java/org/cnodejs/android/md/activity/MainActivity.java
+++ app/src/main/java/org/cnodejs/android/md/activity/MainActivity.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 
 import org.cnodejs.android.md.R;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
 }
