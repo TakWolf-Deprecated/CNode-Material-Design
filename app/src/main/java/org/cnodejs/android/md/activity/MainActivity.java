cd72a8f20f5c71dc13ad1b6e096427bd3b6ee0ce
TakWolf <takwolf@foxmail.com>
2015-08-03 05:19:08 +0800

    添加首页获取消息数目策略

--- app/src/main/java/org/cnodejs/android/md/activity/MainActivity.java
+++ app/src/main/java/org/cnodejs/android/md/activity/MainActivity.java
@@ -172,6 +172,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         public void onDrawerOpened(View drawerView) {
             updateUserInfoViews();
             getUserAsycnTask();
+            getMessageCountAsyncTask();
         }
 
     };
