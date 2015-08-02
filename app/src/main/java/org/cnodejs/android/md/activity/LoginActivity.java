ee3eb46f8494d619539ceaa94d88b8193eaa66c3
TakWolf <takwolf@foxmail.com>
2015-08-02 22:42:26 +0800

    登录界面

--- app/src/main/java/org/cnodejs/android/md/activity/LoginActivity.java
+++ app/src/main/java/org/cnodejs/android/md/activity/LoginActivity.java
@@ -2,15 +2,26 @@ package org.cnodejs.android.md.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import org.cnodejs.android.md.R;
+import org.cnodejs.android.md.listener.NavigationFinishClickListener;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.login_toolbar)
+    protected Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        toolbar.setNavigationOnClickListener(new NavigationFinishClickListener(this));
     }
 
 }
