0877311df0c9f5e66b178de5deb6ffdad36eb665
TakWolf <takwolf@foxmail.com>
2015-08-03 22:07:45 +0800

    消息界面获取初步

--- app/src/main/java/org/cnodejs/android/md/activity/NotificationActivity.java
+++ app/src/main/java/org/cnodejs/android/md/activity/NotificationActivity.java
@@ -1,19 +1,51 @@
 package org.cnodejs.android.md.activity;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import org.cnodejs.android.md.R;
+import org.cnodejs.android.md.adapter.NotificationAdapter;
 import org.cnodejs.android.md.listener.NavigationFinishClickListener;
+import org.cnodejs.android.md.model.api.ApiClient;
+import org.cnodejs.android.md.model.entity.Message;
+import org.cnodejs.android.md.model.entity.Notification;
+import org.cnodejs.android.md.model.entity.Result;
+import org.cnodejs.android.md.storage.LoginShared;
+import org.cnodejs.android.md.util.HandlerUtils;
 
-import butterknife.Bind;
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.ButterKnife;
+import butterknife.Bind;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
-public class NotificationActivity extends AppCompatActivity {
+public class NotificationActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener, SwipeRefreshLayout.OnRefreshListener {
 
     @Bind(R.id.notification_toolbar)
     protected Toolbar toolbar;
+
+    @Bind(R.id.notification_refresh_layout)
+    protected SwipeRefreshLayout refreshLayout;
+
+    @Bind(R.id.notification_recycler_view)
+    protected RecyclerView recyclerView;
+
+    @Bind(R.id.notification_layout_no_data)
+    protected ViewGroup layoutNoData;
+
+    private NotificationAdapter adapter;
+    private List<Message> messageList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +54,65 @@ public class NotificationActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         toolbar.setNavigationOnClickListener(new NavigationFinishClickListener(this));
+        toolbar.inflateMenu(R.menu.notification);
+        toolbar.setOnMenuItemClickListener(this);
+
+        refreshLayout.setColorSchemeResources(R.color.red_light, R.color.green_light, R.color.blue_light, R.color.orange_light);
+        refreshLayout.setOnRefreshListener(this);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new NotificationAdapter(this, messageList);
+        recyclerView.setAdapter(adapter);
+
+        HandlerUtils.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                refreshLayout.setRefreshing(true);
+                onRefresh();
+            }
+
+        }, 100); // refreshLayout无法直接在onCreate中设置刷新状态
+    }
+
+    @Override
+    public void onRefresh() {
+        ApiClient.service.getMessages(LoginShared.getAccessToken(this), new Callback<Result<Notification>>() {
+
+            @Override
+            public void success(Result<Notification> result, Response response) {
+                if (!isFinishing()) {
+                    messageList.clear();
+                    messageList.addAll(result.getData().getHasNotReadMessages());
+                    messageList.addAll(result.getData().getHasReadMessages());
+                    adapter.notifyDataSetChanged();
+                    layoutNoData.setVisibility(messageList.size() == 0 ? View.VISIBLE : View.GONE);
+                    refreshLayout.setRefreshing(false);
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                if (!isFinishing()) {
+                    Toast.makeText(NotificationActivity.this, R.string.data_load_faild, Toast.LENGTH_SHORT).show();
+                    refreshLayout.setRefreshing(false);
+                }
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_done_all:
+
+                // TODO
+
+                return true;
+            default:
+                return false;
+        }
     }
 
 }
---
+++ app/src/main/java/org/cnodejs/android/md/adapter/NotificationAdapter.java
@@ -0,0 +1,123 @@
+package org.cnodejs.android.md.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.cnodejs.android.md.R;
+import org.cnodejs.android.md.activity.TopicActivity;
+import org.cnodejs.android.md.activity.UserDetailActivity;
+import org.cnodejs.android.md.model.api.ApiClient;
+import org.cnodejs.android.md.model.entity.Message;
+import org.cnodejs.android.md.model.entity.MessageType;
+import org.cnodejs.android.md.util.FormatUtils;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
+
+    private Context context;
+    private LayoutInflater inflater;
+    private List<Message> messageList;
+
+    public NotificationAdapter(Context context, @NonNull List<Message> messageList) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+        this.messageList = messageList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(inflater.inflate(R.layout.activity_notification_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.update(position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.notification_item_img_avatar)
+        protected ImageView imgAvatar;
+
+        @Bind(R.id.notification_item_tv_from)
+        protected TextView tvFrom;
+
+        @Bind(R.id.notification_item_tv_time)
+        protected TextView tvTime;
+
+        @Bind(R.id.notification_item_tv_action)
+        protected TextView tvAction;
+
+        @Bind(R.id.notification_item_tv_reply_content)
+        protected TextView tvReplyContent;
+
+        @Bind(R.id.notification_item_tv_topic_title)
+        protected TextView tvTopicTitle;
+
+        private Message message;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void update(int position) {
+            message = messageList.get(position);
+
+            Picasso.with(context).load(ApiClient.ROOT_HOST + message.getAuthor().getAvatarUrl()).error(R.drawable.image_default).into(imgAvatar);
+            tvFrom.setText(message.getAuthor().getLoginName());
+            tvAction.setText(message.getType() == MessageType.at ? "在回复中@了您" : "回复了您的话题");
+            tvTime.setText(FormatUtils.getRecentlyTimeFormatText(message.getReply().getCreateAt()));
+
+            // TODO 需要渲染
+            tvReplyContent.setText(message.getReply().getContent());
+
+            tvTopicTitle.setText("原话题：" + message.getTopic().getTitle());
+
+
+
+
+            // 已读未读状态
+            tvTime.setTextColor(context.getResources().getColor(message.isRead() ? R.color.text_color_secondary : R.color.color_accent));
+            tvFrom.getPaint().setFakeBoldText(!message.isRead());
+            tvFrom.setTextColor(context.getResources().getColor(message.isRead() ? R.color.text_color_primary : R.color.color_accent));
+            tvAction.getPaint().setFakeBoldText(!message.isRead());
+            tvAction.setTextColor(context.getResources().getColor(message.isRead() ? R.color.text_color_secondary : R.color.text_color_primary));
+        }
+
+        @OnClick(R.id.notification_item_img_avatar)
+        protected void onBtnAvatarClick() {
+            Intent intent = new Intent(context, UserDetailActivity.class);
+            intent.putExtra("loginName", message.getAuthor().getLoginName());
+            context.startActivity(intent);
+        }
+
+        @OnClick(R.id.notification_item_btn_item)
+        protected void onBtnItemClick() {
+            Intent intent = new Intent(context, TopicActivity.class);
+            intent.putExtra("topicId", message.getTopic().getId());
+            context.startActivity(intent);
+        }
+
+    }
+
+}
