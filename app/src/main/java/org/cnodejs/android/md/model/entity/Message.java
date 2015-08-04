c845737d4bdf325fb5ba58dc9ae5f794b65b257a
TakWolf <takwolf@foxmail.com>
2015-08-04 22:12:26 +0800

    代码整理

--- app/src/main/java/org/cnodejs/android/md/model/entity/Message.java
+++ app/src/main/java/org/cnodejs/android/md/model/entity/Message.java
@@ -15,9 +15,9 @@ public class Message {
 
     private Author author;
 
-    private TopicSimple topic; // 这里不含Author字段，注意
+    private TopicSimple topic; // TODO 这里不含Author字段，注意
 
-    private Reply reply; // 这里不含Author字段，注意
+    private Reply reply; // TODO 这里不含Author字段，注意
 
     public String getId() {
         return id;
@@ -29,7 +29,7 @@ public class Message {
 
     @NonNull
     public MessageType getType() {
-        return type == null ? MessageType.normal : type; // 保证返回不为空
+        return type == null ? MessageType.normal : type; // TODO 保证返回不为空
     }
 
     public void setType(MessageType type) {
