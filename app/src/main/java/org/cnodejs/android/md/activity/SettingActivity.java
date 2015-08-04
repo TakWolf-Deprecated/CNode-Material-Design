3e4c93449c8d3fef467b20d0a713c284e53b5912
TakWolf <takwolf@foxmail.com>
2015-08-04 10:12:40 +0800

    设置界面

--- app/src/main/java/org/cnodejs/android/md/activity/SettingActivity.java
+++ app/src/main/java/org/cnodejs/android/md/activity/SettingActivity.java
@@ -3,17 +3,30 @@ package org.cnodejs.android.md.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import com.rey.material.widget.Switch;
 
 import org.cnodejs.android.md.R;
 import org.cnodejs.android.md.listener.NavigationFinishClickListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SettingActivity extends AppCompatActivity {
 
     @Bind(R.id.setting_toolbar)
     protected Toolbar toolbar;
+
+    @Bind(R.id.setting_switch_notification)
+    protected Switch switchNotification;
+
+    @Bind(R.id.setting_switch_topic_sign)
+    protected Switch switchTopicSign;
+
+    @Bind(R.id.setting_btn_modify_topic_sign)
+    protected TextView btnModifyTopicSign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,22 @@ public class SettingActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         toolbar.setNavigationOnClickListener(new NavigationFinishClickListener(this));
+    }
+
+    @OnClick(R.id.setting_btn_notification)
+    protected void onBtnNotificationClick() {
+        switchNotification.toggle();
+    }
+
+    @OnClick(R.id.setting_btn_topic_sign)
+    protected void onBtnTopicSignClick() {
+        switchTopicSign.toggle();
+        btnModifyTopicSign.setEnabled(switchTopicSign.isChecked());
+    }
+
+    @OnClick(R.id.setting_btn_modify_topic_sign)
+    protected void onBtnModifyTopicSignClick() {
+
     }
 
 }
