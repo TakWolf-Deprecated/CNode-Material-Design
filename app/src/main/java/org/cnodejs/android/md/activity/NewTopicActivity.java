58405a9cd3ffdb7125f09ab8bf85935f6c6bbd66
TakWolf <takwolf@foxmail.com>
2015-08-04 22:11:00 +0800

    代码整理

--- app/src/main/java/org/cnodejs/android/md/activity/NewTopicActivity.java
+++ app/src/main/java/org/cnodejs/android/md/activity/NewTopicActivity.java
@@ -1,23 +1,13 @@
 package org.cnodejs.android.md.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.InputFilter;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -29,7 +19,6 @@ import org.cnodejs.android.md.storage.LoginShared;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
 public class NewTopicActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
