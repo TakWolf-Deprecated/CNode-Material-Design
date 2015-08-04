11cc9cdd63843e89d1acc908fc625d5f843fe566
TakWolf <takwolf@foxmail.com>
2015-08-04 11:37:52 +0800

    新话题页面添加发送按钮

--- app/src/main/java/org/cnodejs/android/md/activity/NewTopicActivity.java
+++ app/src/main/java/org/cnodejs/android/md/activity/NewTopicActivity.java
@@ -3,6 +3,7 @@ package org.cnodejs.android.md.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import org.cnodejs.android.md.R;
 import org.cnodejs.android.md.listener.NavigationFinishClickListener;
@@ -10,7 +11,7 @@ import org.cnodejs.android.md.listener.NavigationFinishClickListener;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class NewTopicActivity extends AppCompatActivity {
+public class NewTopicActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
 
     @Bind(R.id.new_topic_toolbar)
     protected Toolbar toolbar;
@@ -22,6 +23,21 @@ public class NewTopicActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         toolbar.setNavigationOnClickListener(new NavigationFinishClickListener(this));
+        toolbar.inflateMenu(R.menu.new_topic);
+        toolbar.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_send:
+
+                // TODO
+
+                return true;
+            default:
+                return false;
+        }
     }
 
 }
