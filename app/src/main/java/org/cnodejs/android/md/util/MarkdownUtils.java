8c7cc0475de6b471c0e096837e0bcfbc1d096027
TakWolf <takwolf@foxmail.com>
2015-08-05 13:24:06 +0800

    markdown webview预览小问题解决

--- app/src/main/java/org/cnodejs/android/md/util/MarkdownUtils.java
+++ app/src/main/java/org/cnodejs/android/md/util/MarkdownUtils.java
@@ -11,8 +11,14 @@ public final class MarkdownUtils {
 
     private static final MarkdownProcessor mp = new MarkdownProcessor();
 
+    public static String cnodeFilter(String text) {
+        text = text.replace("](/user/", "](https://cnodejs.org/user/"); // TODO 替换@用户协议
+        text = text.replace("](//dn-cnode.qbox.me/", "](https://dn-cnode.qbox.me/"); // TODO 替换CNode CDN图片路径
+        return text;
+    }
+
     public static String markdown(String text) {
-        return mp.markdown(text.replace("//dn-cnode.qbox.me/", "https://dn-cnode.qbox.me/")); // TODO 替换CNode CDN图片路径
+        return mp.markdown(cnodeFilter(text));
     }
 
     public static Spanned spanned(String text) {
