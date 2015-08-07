9693d97861a0ed0e3fdd20bf9822d5f2f3c5e9f2
TakWolf <takwolf@foxmail.com>
2015-08-07 09:52:35 +0800

    webview内嵌链接增加话题协议

--- app/src/main/java/org/cnodejs/android/md/listener/WebViewContentClient.java
+++ app/src/main/java/org/cnodejs/android/md/listener/WebViewContentClient.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import org.cnodejs.android.md.activity.TopicActivity;
 import org.cnodejs.android.md.activity.UserDetailActivity;
 import org.cnodejs.android.md.util.ShipUtils;
 
@@ -18,9 +19,13 @@ public class WebViewContentClient extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView webView, String url) {
-        if (url.startsWith("https://cnodejs.org/user/")) { // 跳转到用户页面
+        if (url.startsWith("https://cnodejs.org/user/")) { // 用户主页协议
             Intent intent = new Intent(context, UserDetailActivity.class);
             intent.putExtra("loginName", url.substring(25));
+            context.startActivity(intent);
+        } else if (url.startsWith("https://cnodejs.org/topic/")) { // 话题主页协议
+            Intent intent = new Intent(context, TopicActivity.class);
+            intent.putExtra("topicId", url.substring(26));
             context.startActivity(intent);
         } else { // 其他连接
             ShipUtils.openUrlByBrowser(context, url);
