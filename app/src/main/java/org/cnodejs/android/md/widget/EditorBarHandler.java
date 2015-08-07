122048918eede286409d8a4e4d00ccc510b1b800
TakWolf <takwolf@foxmail.com>
2015-08-07 11:21:14 +0800

    插入图片

--- app/src/main/java/org/cnodejs/android/md/widget/EditorBarHandler.java
+++ app/src/main/java/org/cnodejs/android/md/widget/EditorBarHandler.java
@@ -133,8 +133,8 @@ public class EditorBarHandler {
     @OnClick(R.id.editor_bar_btn_insert_photo)
     protected void onBtnInsertPhotoClick() {
         edtContent.requestFocus();
-        edtContent.getText().insert(edtContent.getSelectionEnd(), " ![Image](http://) ");
-        edtContent.setSelection(edtContent.getSelectionEnd() - 2);
+        edtContent.getText().insert(edtContent.getSelectionEnd(), " ![Image](http://resource) ");
+        edtContent.setSelection(edtContent.getSelectionEnd() - 10, edtContent.getSelectionEnd() - 2);
     }
 
     /**
