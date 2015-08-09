b9565b428f8b7cbadcd9da5dda766c5336ad16f1
TakWolf <takwolf@foxmail.com>
2015-08-09 13:28:37 +0800

    添加崩溃日志监控逻辑

--- app/src/main/java/org/cnodejs/android/md/app/AppController.java
+++ app/src/main/java/org/cnodejs/android/md/app/AppController.java
@@ -1,12 +1,28 @@
 package org.cnodejs.android.md.app;
 
 import android.app.Application;
+import android.content.Intent;
+import android.os.Bundle;
 
-public class AppController extends Application {
+import org.cnodejs.android.md.activity.CrashLogActivity;
+
+public class AppController extends Application implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Thread.setDefaultUncaughtExceptionHandler(this);
+    }
+
+    @Override
+    public void uncaughtException(Thread thread, final Throwable e) {
+        Intent intent = new Intent(this, CrashLogActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("e", e);
+        intent.putExtras(bundle);
+        startActivity(intent);
+        System.exit(1);
     }
 
 }
