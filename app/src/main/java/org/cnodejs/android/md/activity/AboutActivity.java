664bd2ac60e7cef5c48a68d5ecc7549cd861ec7b
TakWolf <takwolf@foxmail.com>
2015-08-14 15:32:29 +0800

    关于页面添加为应用评分

--- app/src/main/java/org/cnodejs/android/md/activity/AboutActivity.java
+++ app/src/main/java/org/cnodejs/android/md/activity/AboutActivity.java
@@ -66,6 +66,11 @@ public class AboutActivity extends AppCompatActivity {
                 "设备信息：Android " + Build.VERSION.RELEASE + " - " + Build.MANUFACTURER + " - " + Build.MODEL + "\n（如果涉及隐私请手动删除这个内容）\n\n");
     }
 
+    @OnClick(R.id.about_btn_open_in_app_store)
+    protected void onBtnOpenInAppStoreClick() {
+        ShipUtils.openInAppStore(this);
+    }
+
     @OnClick(R.id.about_btn_open_source_license)
     protected void onBtnOpenSourceLicenseClick() {
         startActivity(new Intent(this, LicenseActivity.class));
