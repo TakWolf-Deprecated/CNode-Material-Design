25b30951a0aad61f9f5eced425c7f3c1e57b4b2b
TakWolf <takwolf@foxmail.com>
2015-08-29 23:45:03 +0800

    okhttp更新

--- app/src/main/java/org/cnodejs/android/md/activity/TopicActivity.java
+++ app/src/main/java/org/cnodejs/android/md/activity/TopicActivity.java
@@ -1,6 +1,5 @@
 package org.cnodejs.android.md.activity;
 
-import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
