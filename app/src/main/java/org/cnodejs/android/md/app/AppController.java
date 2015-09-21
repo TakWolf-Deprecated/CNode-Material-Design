4487253f6698afb78aae52bb24fd7d48841b97c5
TakWolf <takwolf@foxmail.com>
2015-09-21 10:39:12 +0800

    appcontroller 初始化策略变更

--- app/src/main/java/org/cnodejs/android/md/app/AppController.java
+++ app/src/main/java/org/cnodejs/android/md/app/AppController.java
@@ -1,6 +1,7 @@
 package org.cnodejs.android.md.app;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -8,10 +9,20 @@ import org.cnodejs.android.md.activity.CrashLogActivity;
 
 public class AppController extends Application implements Thread.UncaughtExceptionHandler {
 
+    private static Context context;
+
+    public static Context getContext() {
+        return context;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        Thread.setDefaultUncaughtExceptionHandler(this);
+        if (context == null) {
+            context = this;
+
+            Thread.setDefaultUncaughtExceptionHandler(this);
+        }
     }
 
     @Override
