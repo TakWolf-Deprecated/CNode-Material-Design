dd0368008187aa4423f6156d4d1378a50359fb8a
TakWolf <takwolf@foxmail.com>
2015-09-24 22:51:01 +0800

    资源名称调整

--- app/src/main/java/org/cnodejs/android/md/ui/activity/UserDetailActivity.java
+++ app/src/main/java/org/cnodejs/android/md/ui/activity/UserDetailActivity.java
@@ -139,7 +139,7 @@ public class UserDetailActivity extends AppCompatActivity {
     }
 
     private void updateUserInfoViews(User user) {
-        Picasso.with(this).load(user.getAvatarUrl()).placeholder(R.drawable.image_default).into(imgAvatar);
+        Picasso.with(this).load(user.getAvatarUrl()).placeholder(R.drawable.image_placeholder).into(imgAvatar);
         tvLoginName.setText(user.getLoginName());
         if (TextUtils.isEmpty(user.getGithubUsername())) {
             tvGithubUsername.setVisibility(View.INVISIBLE);
