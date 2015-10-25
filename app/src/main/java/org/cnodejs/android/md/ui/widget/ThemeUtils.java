e5aecd6311dd57d98223979fff7f364b84de43eb
TakWolf <takwolf@foxmail.com>
2015-10-26 01:19:08 +0800

    themeUtils添加主题方法

--- app/src/main/java/org/cnodejs/android/md/ui/widget/ThemeUtils.java
+++ app/src/main/java/org/cnodejs/android/md/ui/widget/ThemeUtils.java
@@ -1,9 +1,15 @@
 package org.cnodejs.android.md.ui.widget;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
+import android.os.Build;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+
+import org.cnodejs.android.md.storage.SettingShared;
 
 public final class ThemeUtils {
 
@@ -15,6 +21,20 @@ public final class ThemeUtils {
             return a.getColor(0, 0);
         } finally {
             a.recycle();
+        }
+    }
+
+    public static void configThemeBeforeOnCreate(Activity activity, @StyleRes int light, @StyleRes int dark) {
+        activity.setTheme(SettingShared.isEnableThemeDark(activity) ? dark : light);
+    }
+
+    public static void recreateActivity(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            activity.recreate();
+        } else {
+            activity.startActivity(new Intent(activity, activity.getClass()));
+            activity.finish();
+            activity.overridePendingTransition(0, 0);
         }
     }
 
