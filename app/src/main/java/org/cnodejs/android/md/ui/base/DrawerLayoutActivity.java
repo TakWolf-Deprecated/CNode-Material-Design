87ca4e9a8a952d839d6297252109b890d595f2fd
TakWolf <takwolf@foxmail.com>
2015-11-11 20:31:57 +0800

    activity抽象

---
+++ app/src/main/java/org/cnodejs/android/md/ui/base/DrawerLayoutActivity.java
@@ -0,0 +1,12 @@
+package org.cnodejs.android.md.ui.base;
+
+import android.os.Bundle;
+
+public class DrawerLayoutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
