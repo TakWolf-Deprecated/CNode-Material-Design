22a7a861875cb5d116c0ff17db73789eb98e8375
TakWolf <takwolf@foxmail.com>
2016-02-26 21:52:02 +0800

    对话框构建工具

---
+++ app/src/main/java/org/cnodejs/android/md/ui/dialog/DialogUtils.java
@@ -0,0 +1,20 @@
+package org.cnodejs.android.md.ui.dialog;
+
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+
+import org.cnodejs.android.md.ui.widget.ThemeUtils;
+
+public final class DialogUtils {
+
+    private DialogUtils() {}
+
+    public static ProgressDialog createProgressDialog(Context context) {
+        return new ProgressDialog(context, ThemeUtils.getDialogThemeRes(context));
+    }
+
+    public static AlertDialog.Builder createAlertDialogBuilder(Context context) {
+        return new AlertDialog.Builder(context, ThemeUtils.getDialogThemeRes(context));
+    }
+
+}
