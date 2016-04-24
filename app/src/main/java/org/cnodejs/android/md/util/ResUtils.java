690d25492bef23ac4879b1ca254845b1e81a5a62
TakWolf <takwolf@foxmail.com>
2016-04-25 05:31:43 +0800

    代码整理

--- app/src/main/java/org/cnodejs/android/md/util/ResUtils.java
+++ app/src/main/java/org/cnodejs/android/md/util/ResUtils.java
@@ -13,21 +13,17 @@ public final class ResUtils {
 
     private ResUtils() {}
 
-    public static String getRawString(@NonNull Context context, @RawRes int rawId) {
-        try {
-            InputStream is = context.getResources().openRawResource(rawId);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-            sb.deleteCharAt(sb.length() - 1);
-            reader.close();
-            return sb.toString();
-        } catch (IOException e) {
-            return "";
+    public static String getRawString(@NonNull Context context, @RawRes int rawId) throws IOException {
+        InputStream is = context.getResources().openRawResource(rawId);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
         }
+        sb.deleteCharAt(sb.length() - 1);
+        reader.close();
+        return sb.toString();
     }
 
 }
