8ba4d0f8c6ee8937801e04fa1321745bc5adb37d
TakWolf <takwolf@foxmail.com>
2016-05-03 10:12:27 +0800

    连接处理逻辑转移

--- app/src/main/java/org/cnodejs/android/md/util/ShipUtils.java
+++ app/src/main/java/org/cnodejs/android/md/util/ShipUtils.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 
 import org.cnodejs.android.md.R;
+import org.cnodejs.android.md.display.activity.TopicActivity;
+import org.cnodejs.android.md.display.activity.UserDetailActivity;
 import org.cnodejs.android.md.display.widget.ToastUtils;
 
 public final class ShipUtils {
@@ -50,6 +52,16 @@ public final class ShipUtils {
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, text);
         context.startActivity(Intent.createChooser(intent, context.getString(R.string.share)));
+    }
+
+    public static void handleLink(Context context, String url) {
+        if (url.startsWith("https://cnodejs.org/user/")) { // 用户主页协议
+            UserDetailActivity.start(context, url.substring(25));
+        } else if (url.startsWith("https://cnodejs.org/topic/")) { // 话题主页协议
+            TopicActivity.start(context, url.substring(26));
+        } else { // 其他连接
+            ShipUtils.openInBrowser(context, url);
+        }
     }
 
 }
