25a8e8a737c07c88cde14fdaedbf7f157a6fc9dd
TakWolf <takwolf@foxmail.com>
2016-05-10 08:20:32 +0800

    deeplinking接收器改为独立一个activity

---
+++ app/src/main/java/org/cnodejs/android/md/display/activity/LinkRouterActivity.java
@@ -0,0 +1,14 @@
+package org.cnodejs.android.md.display.activity;
+
+import android.os.Bundle;
+
+import org.cnodejs.android.md.display.base.BaseActivity;
+
+public class LinkRouterActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
