e4ced818ef261d901a790383d6c2aa0ff4dbb8a7
TakWolf <takwolf@foxmail.com>
2016-05-15 16:15:24 +0800

    话题兼容页面，修复权限验证问题

--- app/src/main/java/org/cnodejs/android/md/display/listener/TopicJavascriptInterface.java
+++ app/src/main/java/org/cnodejs/android/md/display/listener/TopicJavascriptInterface.java
@@ -1,12 +1,16 @@
 package org.cnodejs.android.md.display.listener;
 
-import android.content.Context;
+import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.webkit.JavascriptInterface;
 
+import org.cnodejs.android.md.R;
+import org.cnodejs.android.md.display.activity.LoginActivity;
 import org.cnodejs.android.md.display.activity.UserDetailActivity;
+import org.cnodejs.android.md.display.util.ToastUtils;
 import org.cnodejs.android.md.display.view.ITopicReplyView;
 import org.cnodejs.android.md.model.entity.Reply;
+import org.cnodejs.android.md.model.storage.LoginShared;
 import org.cnodejs.android.md.model.util.EntityUtils;
 import org.cnodejs.android.md.presenter.contract.ITopicHeaderPresenter;
 import org.cnodejs.android.md.presenter.contract.ITopicItemReplyPresenter;
@@ -16,13 +20,13 @@ public final class TopicJavascriptInterface {
 
     public static final String NAME = "topicBridge";
 
-    private final Context context;
+    private final Activity activity;
     private final ITopicReplyView topicReplyView;
     private final ITopicHeaderPresenter topicHeaderPresenter;
     private final ITopicItemReplyPresenter topicItemReplyPresenter;
 
-    public TopicJavascriptInterface(@NonNull Context context, @NonNull ITopicReplyView topicReplyView, @NonNull ITopicHeaderPresenter topicHeaderPresenter, @NonNull ITopicItemReplyPresenter topicItemReplyPresenter) {
-        this.context = context;
+    public TopicJavascriptInterface(@NonNull Activity activity, @NonNull ITopicReplyView topicReplyView, @NonNull ITopicHeaderPresenter topicHeaderPresenter, @NonNull ITopicItemReplyPresenter topicItemReplyPresenter) {
+        this.activity = activity;
         this.topicReplyView = topicReplyView;
         this.topicHeaderPresenter = topicHeaderPresenter;
         this.topicItemReplyPresenter = topicItemReplyPresenter;
@@ -30,36 +34,48 @@ public final class TopicJavascriptInterface {
 
     @JavascriptInterface
     public void collectTopic(String topicId) {
-        topicHeaderPresenter.collectTopicAsyncTask(topicId);
+        if (LoginActivity.startForResultWithAccessTokenCheck(activity)) {
+            topicHeaderPresenter.collectTopicAsyncTask(topicId);
+        }
     }
 
     @JavascriptInterface
     public void decollectTopic(String topicId) {
-        topicHeaderPresenter.decollectTopicAsyncTask(topicId);
+        if (LoginActivity.startForResultWithAccessTokenCheck(activity)) {
+            topicHeaderPresenter.decollectTopicAsyncTask(topicId);
+        }
     }
 
     @JavascriptInterface
     public void upReply(String replyJson) {
-        Reply reply = EntityUtils.gson.fromJson(replyJson, Reply.class);
-        topicItemReplyPresenter.upReplyAsyncTask(reply);
+        if (LoginActivity.startForResultWithAccessTokenCheck(activity)) {
+            Reply reply = EntityUtils.gson.fromJson(replyJson, Reply.class);
+            if (reply.getAuthor().getLoginName().equals(LoginShared.getLoginName(activity))) {
+                ToastUtils.with(activity).show(R.string.can_not_up_yourself_reply);
+            } else {
+                topicItemReplyPresenter.upReplyAsyncTask(reply);
+            }
+        }
     }
 
     @JavascriptInterface
-    public void at(String targetJson, final int targetPosition) {
-        final Reply target = EntityUtils.gson.fromJson(targetJson, Reply.class);
-        HandlerUtils.post(new Runnable() {
+    public void at(final String targetJson, final int targetPosition) {
+        if (LoginActivity.startForResultWithAccessTokenCheck(activity)) {
+            HandlerUtils.post(new Runnable() {
 
-            @Override
-            public void run() {
-                topicReplyView.onAt(target, targetPosition);
-            }
+                @Override
+                public void run() {
+                    Reply target = EntityUtils.gson.fromJson(targetJson, Reply.class);
+                    topicReplyView.onAt(target, targetPosition);
+                }
 
-        });
+            });
+        }
     }
 
     @JavascriptInterface
     public void openUser(String loginName) {
-        UserDetailActivity.start(context, loginName);
+        UserDetailActivity.start(activity, loginName);
     }
 
 }
