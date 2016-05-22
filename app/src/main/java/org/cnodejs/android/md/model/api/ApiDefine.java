5af6ae0c83d85491dcfe2bce879280c491c8c45a
TakWolf <takwolf@foxmail.com>
2016-05-23 03:10:06 +0800

    代码整理

--- app/src/main/java/org/cnodejs/android/md/model/api/ApiDefine.java
+++ app/src/main/java/org/cnodejs/android/md/model/api/ApiDefine.java
@@ -4,12 +4,12 @@ public final class ApiDefine {
 
     private ApiDefine() {}
 
-    public static final String HOST = "https://cnodejs.org";
-    public static final String API_BASE_URL = HOST + "/api/v1/";
+    public static final String HOST_BASE_URL = "https://cnodejs.org";
+    public static final String API_BASE_URL = HOST_BASE_URL + "/api/v1/";
     public static final String USER_PATH_PREFIX = "/user/";
-    public static final String USER_LINK_URL_PREFIX = HOST + USER_PATH_PREFIX;
+    public static final String USER_LINK_URL_PREFIX = HOST_BASE_URL + USER_PATH_PREFIX;
     public static final String TOPIC_PATH_PREFIX = "/topic/";
-    public static final String TOPIC_LINK_URL_PREFIX = HOST + TOPIC_PATH_PREFIX;
+    public static final String TOPIC_LINK_URL_PREFIX = HOST_BASE_URL + TOPIC_PATH_PREFIX;
 
     public static final boolean MD_RENDER = true; // 使用服务端Markdown渲染可以轻微提升性能
 
