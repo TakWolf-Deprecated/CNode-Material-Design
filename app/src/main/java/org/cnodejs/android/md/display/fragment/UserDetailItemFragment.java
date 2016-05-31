51543250cdf8e35251ae418f81bd64d39d69a619
TakWolf <takwolf@foxmail.com>
2016-05-31 23:47:48 +0800

    布局资源重命名

--- app/src/main/java/org/cnodejs/android/md/display/fragment/UserDetailItemFragment.java
+++ app/src/main/java/org/cnodejs/android/md/display/fragment/UserDetailItemFragment.java
@@ -28,7 +28,7 @@ public class UserDetailItemFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_user_detail_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_user_detail_item, container, false);
     }
 
     @Override
