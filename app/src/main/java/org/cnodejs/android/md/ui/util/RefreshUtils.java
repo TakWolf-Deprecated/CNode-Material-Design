e283e3c03b9ec0fbdb5cb5206f4a4a6820a07858
TakWolf <takwolf@foxmail.com>
2017-06-10 21:14:51 +0800

    调整颜色

--- app/src/main/java/org/cnodejs/android/md/ui/util/RefreshUtils.java
+++ app/src/main/java/org/cnodejs/android/md/ui/util/RefreshUtils.java
@@ -10,7 +10,7 @@ public final class RefreshUtils {
     private RefreshUtils() {}
 
     public static void init(@NonNull SwipeRefreshLayout refreshLayout, @NonNull SwipeRefreshLayout.OnRefreshListener refreshListener) {
-        refreshLayout.setColorSchemeResources(R.color.red_light, R.color.green_light, R.color.blue_light, R.color.orange_light);
+        refreshLayout.setColorSchemeResources(R.color.material_red, R.color.material_green, R.color.material_blue, R.color.material_yellow);
         refreshLayout.setOnRefreshListener(refreshListener);
     }
 
