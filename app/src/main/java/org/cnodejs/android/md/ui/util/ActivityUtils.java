b7e265aec9f0156b4d307b4e531e1385179e7ef0
TakWolf <takwolf@foxmail.com>
2017-06-10 20:42:21 +0800

    移除 recreateDelayed

--- app/src/main/java/org/cnodejs/android/md/ui/util/ActivityUtils.java
+++ app/src/main/java/org/cnodejs/android/md/ui/util/ActivityUtils.java
@@ -1,11 +1,7 @@
 package org.cnodejs.android.md.ui.util;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Build;
-import android.support.annotation.NonNull;
-
-import org.cnodejs.android.md.util.HandlerUtils;
 
 public final class ActivityUtils {
 
@@ -13,17 +9,6 @@ public final class ActivityUtils {
 
     public static boolean isAlive(Activity activity) {
         return activity != null && !(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && activity.isDestroyed()) && !activity.isFinishing();
-    }
-
-    public static void recreateDelayed(@NonNull final Activity activity) {
-        HandlerUtils.handler.post(new Runnable() {
-
-            @Override
-            public void run() {
-                activity.recreate();
-            }
-
-        });
     }
 
 }
