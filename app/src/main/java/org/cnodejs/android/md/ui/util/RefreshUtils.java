d983ca4618ee238211cb78afe48dc06c7cf66991
TakWolf <takwolf@foxmail.com>
2017-06-10 21:09:30 +0800

    移除下拉刷新兼容

--- app/src/main/java/org/cnodejs/android/md/ui/util/RefreshUtils.java
+++ app/src/main/java/org/cnodejs/android/md/ui/util/RefreshUtils.java
@@ -4,7 +4,6 @@ import android.support.annotation.NonNull;
 import android.support.v4.widget.SwipeRefreshLayout;
 
 import org.cnodejs.android.md.R;
-import org.cnodejs.android.md.util.HandlerUtils;
 
 public final class RefreshUtils {
 
@@ -15,19 +14,9 @@ public final class RefreshUtils {
         refreshLayout.setOnRefreshListener(refreshListener);
     }
 
-    /**
-     * TODO SwipeRefreshLayout 无法直接在 onCreate 中设置刷新状态
-     */
     public static void refresh(@NonNull final SwipeRefreshLayout refreshLayout, @NonNull final SwipeRefreshLayout.OnRefreshListener refreshListener) {
-        HandlerUtils.handler.postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                refreshLayout.setRefreshing(true);
-                refreshListener.onRefresh();
-            }
-
-        }, 100);
+        refreshLayout.setRefreshing(true);
+        refreshListener.onRefresh();
     }
 
 }
