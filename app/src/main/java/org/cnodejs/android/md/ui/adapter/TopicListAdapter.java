7a8e104857f26f17642aed4da8863690a15a355f
TakWolf <takwolf@foxmail.com>
2017-06-15 11:23:34 +0800

    首页列表样式调整，添加摘要

--- app/src/main/java/org/cnodejs/android/md/ui/adapter/TopicListAdapter.java
+++ app/src/main/java/org/cnodejs/android/md/ui/adapter/TopicListAdapter.java
@@ -61,8 +61,20 @@ public class TopicListAdapter extends RecyclerView.Adapter<TopicListAdapter.View
         @BindView(R.id.ctv_tab)
         CheckedTextView ctvTab;
 
+        @BindView(R.id.tv_reply_count)
+        TextView tvReplyCount;
+
+        @BindView(R.id.tv_visit_count)
+        TextView tvVisitCount;
+
+        @BindView(R.id.tv_last_reply_time)
+        TextView tvLastReplyTime;
+
         @BindView(R.id.tv_title)
         TextView tvTitle;
+
+        @BindView(R.id.tv_summary)
+        TextView tvSummary;
 
         @BindView(R.id.img_avatar)
         ImageView imgAvatar;
@@ -72,15 +84,6 @@ public class TopicListAdapter extends RecyclerView.Adapter<TopicListAdapter.View
 
         @BindView(R.id.tv_create_time)
         TextView tvCreateTime;
-
-        @BindView(R.id.tv_reply_count)
-        TextView tvReplyCount;
-
-        @BindView(R.id.tv_visit_count)
-        TextView tvVisitCount;
-
-        @BindView(R.id.tv_last_reply_time)
-        TextView tvLastReplyTime;
 
         @BindView(R.id.icon_good)
         View iconGood;
@@ -95,26 +98,27 @@ public class TopicListAdapter extends RecyclerView.Adapter<TopicListAdapter.View
         void update(@NonNull Topic topic) {
             this.topic = topic;
 
-            tvTitle.setText(topic.getTitle());
             ctvTab.setText(topic.isTop() ? R.string.tab_top : topic.getTab().getNameId());
             ctvTab.setChecked(topic.isTop());
-            Glide.with(activity).load(topic.getAuthor().getAvatarUrl()).placeholder(R.drawable.image_placeholder).dontAnimate().into(imgAvatar);
-            tvAuthor.setText(topic.getAuthor().getLoginName());
-            tvCreateTime.setText(activity.getString(R.string.create_at_$s, topic.getCreateAt().toString("yyyy-MM-dd HH:mm:ss")));
             tvReplyCount.setText(String.valueOf(topic.getReplyCount()));
             tvVisitCount.setText(String.valueOf(topic.getVisitCount()));
             tvLastReplyTime.setText(FormatUtils.getRelativeTimeSpanString(topic.getLastReplyAt()));
+            tvTitle.setText(topic.getTitle());
+            tvSummary.setText(topic.getContentSummary());
+            Glide.with(activity).load(topic.getAuthor().getAvatarUrl()).placeholder(R.drawable.image_placeholder).dontAnimate().into(imgAvatar);
+            tvAuthor.setText(topic.getAuthor().getLoginName());
+            tvCreateTime.setText(activity.getString(R.string.create_at_$s, topic.getCreateAt().toString("yyyy-MM-dd HH:mm:ss")));
             iconGood.setVisibility(topic.isGood() ? View.VISIBLE : View.GONE);
         }
 
-        @OnClick(R.id.img_avatar)
-        void onBtnAvatarClick() {
-            UserDetailActivity.startWithTransitionAnimation(activity, topic.getAuthor().getLoginName(), imgAvatar, topic.getAuthor().getAvatarUrl());
+        @OnClick(R.id.btn_topic)
+        void onBtnTopicClick() {
+            Navigator.TopicWithAutoCompat.start(activity, topic);
         }
 
-        @OnClick(R.id.btn_item)
-        void onBtnItemClick() {
-            Navigator.TopicWithAutoCompat.start(activity, topic);
+        @OnClick(R.id.btn_user)
+        void onBtnUserClick() {
+            UserDetailActivity.startWithTransitionAnimation(activity, topic.getAuthor().getLoginName(), imgAvatar, topic.getAuthor().getAvatarUrl());
         }
 
     }
