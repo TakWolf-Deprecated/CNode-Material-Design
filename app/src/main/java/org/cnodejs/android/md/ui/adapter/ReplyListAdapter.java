105a90de772dc990baaa7902e1e60892edab09de
TakWolf <takwolf@foxmail.com>
2018-01-29 14:59:47 +0800

    代码整理

--- app/src/main/java/org/cnodejs/android/md/ui/adapter/ReplyListAdapter.java
+++ app/src/main/java/org/cnodejs/android/md/ui/adapter/ReplyListAdapter.java
@@ -2,7 +2,6 @@ package org.cnodejs.android.md.ui.adapter;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
