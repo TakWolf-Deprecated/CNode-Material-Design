be0d35e595a2500007acd2acbc4180b41f6e33ed
TakWolf <takwolf@foxmail.com>
2022-02-06 01:10:53 +0800

    升级构建

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.1.0" apply false
-    id("com.android.library") version "7.1.0" apply false
+    id("com.android.application") version "7.1.1" apply false
+    id("com.android.library") version "7.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
 }
 
