b865e3cf12ce09c044bc727e7e006d973c07601b
TakWolf <takwolf@foxmail.com>
2022-02-09 14:09:15 +0800

    定义 LoginViewModel

--- app/src/main/java/org/cnodejs/android/md/ui/fragment/LoginFragment.kt
+++ app/src/main/java/org/cnodejs/android/md/ui/fragment/LoginFragment.kt
@@ -5,13 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import org.cnodejs.android.md.R
 import org.cnodejs.android.md.databinding.FragmentLoginBinding
 import org.cnodejs.android.md.util.NavUtils
-import org.cnodejs.android.md.vm.AccountViewModel
+import org.cnodejs.android.md.vm.LoginViewModel
 
-class LoginFragment : Fragment() {
+class LoginFragment : BaseFragment() {
     companion object {
         fun open(fragment: Fragment) {
             NavUtils.push(fragment, R.id.fragment_login)
@@ -25,7 +25,8 @@ class LoginFragment : Fragment() {
     ): View {
         val binding = FragmentLoginBinding.inflate(inflater, container, false)
 
-        val accountViewModel: AccountViewModel by activityViewModels()
+        val loginViewModel: LoginViewModel by viewModels()
+        observeBaseLiveHolder(loginViewModel.baseLiveHolder)
 
         binding.toolbar.setNavigationOnClickListener {
             NavUtils.back(this)
---
+++ app/src/main/java/org/cnodejs/android/md/vm/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package org.cnodejs.android.md.vm
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import org.cnodejs.android.md.vm.holder.BaseLiveHolder
+
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+    val baseLiveHolder = BaseLiveHolder()
+}
