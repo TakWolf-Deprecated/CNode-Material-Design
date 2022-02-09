c14cdbb65301b4dfb62153015f33831fc67af5ac
TakWolf <takwolf@foxmail.com>
2022-02-10 04:07:25 +0800

    统一抽象格式化工具

--- app/src/main/java/org/cnodejs/android/md/util/HtmlUtils.kt
+++
@@ -1,9 +0,0 @@
-package org.cnodejs.android.md.util
-
-import org.jsoup.Jsoup
-
-object HtmlUtils {
-    fun getText(html: String): String {
-        return Jsoup.parseBodyFragment(html).body().text().trim()
-    }
-}
