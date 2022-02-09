e27cbc0be69459adb738677df14770a7c7c2481a
TakWolf <takwolf@foxmail.com>
2022-02-10 04:57:16 +0800

    首页列表分页完成

--- app/src/main/java/org/cnodejs/android/md/ui/fragment/MainFragment.kt
+++ app/src/main/java/org/cnodejs/android/md/ui/fragment/MainFragment.kt
@@ -1,24 +1,28 @@
 package org.cnodejs.android.md.ui.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.BundleCompat
+import androidx.annotation.ColorInt
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
 import org.cnodejs.android.md.R
 import org.cnodejs.android.md.databinding.FragmentMainBinding
-import org.cnodejs.android.md.model.entity.Account
-import org.cnodejs.android.md.util.NavUtils
+import org.cnodejs.android.md.ui.adapter.TopicListAdapter
+import org.cnodejs.android.md.ui.holder.LoadMoreFooter
 import org.cnodejs.android.md.util.OnDoubleClickListener
 import org.cnodejs.android.md.vm.AccountViewModel
+import org.cnodejs.android.md.vm.MainViewModel
 import org.cnodejs.android.md.vm.SettingViewModel
+import org.cnodejs.android.md.vm.holder.setupView
 
-class MainFragment : Fragment() {
+class MainFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,16 +30,22 @@ class MainFragment : Fragment() {
     ): View {
         val binding = FragmentMainBinding.inflate(inflater, container, false)
 
+        val a = requireContext().obtainStyledAttributes(intArrayOf(android.R.attr.colorAccent))
+        @ColorInt val colorAccent = a.getColor(0, Color.TRANSPARENT)
+        a.recycle()
+
         val accountViewModel: AccountViewModel by activityViewModels()
         val settingViewModel: SettingViewModel by activityViewModels()
+        val mainViewModel: MainViewModel by viewModels()
+        observeBaseLiveHolder(mainViewModel.baseLiveHolder)
 
         accountViewModel.accountData.observe(viewLifecycleOwner) {
             it?.also { account ->
-                binding.navLayout.imgLoginAvatar.load(account.avatarUrl) {
+                binding.navLayout.imgLoginAvatar.load(account.getCompatAvatarUrl()) {
                     placeholder(R.drawable.image_placeholder)
                 }
                 binding.navLayout.tvLoginName.text = account.loginName
-                binding.navLayout.tvScore.text = getString(R.string.score__d_, account.score)
+                binding.navLayout.tvScore.text = getString(R.string.score_d, account.score)
                 binding.navLayout.btnLogout.visibility = View.VISIBLE
             } ?: run {
                 binding.navLayout.imgLoginAvatar.load(R.drawable.image_placeholder)
@@ -57,6 +67,10 @@ class MainFragment : Fragment() {
             }
         }
 
+        mainViewModel.topicPagingLiveHolder.tabData.observe(viewLifecycleOwner) {
+            // TODO
+        }
+
         binding.drawerLayout.addDrawerListener(object : DrawerLayout.SimpleDrawerListener() {
             // TODO
         })
@@ -69,6 +83,14 @@ class MainFragment : Fragment() {
                 binding.contentLayout.recyclerView.scrollToPosition(0)
             }
         })
+
+        binding.contentLayout.refreshLayout.setColorSchemeColors(colorAccent)
+        binding.contentLayout.recyclerView.layoutManager = LinearLayoutManager(context)
+        val loadMoreFooter = LoadMoreFooter.create(binding.contentLayout.recyclerView)
+        val adapter = TopicListAdapter()
+        mainViewModel.topicPagingLiveHolder.setupView(viewLifecycleOwner, adapter, binding.contentLayout.refreshLayout, loadMoreFooter)
+        loadMoreFooter.addToRecyclerView(binding.contentLayout.recyclerView)
+        binding.contentLayout.recyclerView.adapter = adapter
 
         val onNavMyInfoClickListener = View.OnClickListener {
             accountViewModel.accountData.value?.also { account ->
---
+++ app/src/main/java/org/cnodejs/android/md/vm/holder/TopicPagingLiveHolder.kt
@@ -0,0 +1,69 @@
+package org.cnodejs.android.md.vm.holder
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import org.cnodejs.android.md.BuildConfig
+import org.cnodejs.android.md.model.api.CNodeClient
+import org.cnodejs.android.md.model.entity.ErrorResult
+import org.cnodejs.android.md.model.entity.Tab
+import org.cnodejs.android.md.model.entity.TopicWithSummary
+
+class TopicPagingLiveHolder(
+    viewModelScope: CoroutineScope,
+    baseLiveHolder: BaseLiveHolder,
+) : PagingLiveHolder<TopicWithSummary, Int>(
+    viewModelScope,
+    baseLiveHolder,
+) {
+    companion object {
+        private const val TAG = "TopicPagingLiveHolder"
+    }
+
+    val tabData = MutableLiveData(Tab.ALL)
+
+    init {
+        refresh()
+    }
+
+    fun getTab(): Tab {
+        return tabData.value ?: Tab.ALL
+    }
+
+    fun switchTab(tab: Tab) {
+        if (getTab() != tab) {
+            tabData.value = tab
+            resetPaging()
+            refresh()
+        }
+    }
+
+    override suspend fun doRefresh(version: Int) {
+        try {
+            val result = CNodeClient.api.getTopics(tab = getTab().queryValue, mdrender = true)
+            val topics = TopicWithSummary.fromList(result.data)
+            refreshSuccess(version, topics, 1, topics.isEmpty())
+        } catch (e: Exception) {
+            if (BuildConfig.DEBUG) {
+                Log.e(TAG, "doRefresh", e)
+            }
+            val errorResult = ErrorResult.from(e)
+            refreshFailure(version, errorResult.message)
+        }
+    }
+
+    override suspend fun doLoadMore(version: Int, pagingParams: Int) {
+        try {
+            val nextPage = pagingParams + 1
+            val result = CNodeClient.api.getTopics(page = nextPage, mdrender = true)
+            val topics = TopicWithSummary.fromList(result.data)
+            loadMoreSuccess(version, topics, nextPage, topics.isEmpty())
+        } catch (e: Exception) {
+            if (BuildConfig.DEBUG) {
+                Log.e(TAG, "doLoadMore", e)
+            }
+            val errorResult = ErrorResult.from(e)
+            refreshFailure(version, errorResult.message)
+        }
+    }
+}
