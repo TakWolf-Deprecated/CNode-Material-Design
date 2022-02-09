1627f6ee257c08d75657efcdcadb5f784a479f10
TakWolf <takwolf@foxmail.com>
2022-02-10 03:11:46 +0800

    实体类 boolean 类型添加 is 前缀

--- app/src/main/java/org/cnodejs/android/md/model/entity/Result.kt
+++ app/src/main/java/org/cnodejs/android/md/model/entity/Result.kt
@@ -5,36 +5,36 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class ApiResult(
-    val success: Boolean,
+    @Json(name = "success") val isSuccessful: Boolean,
 )
 
 @JsonClass(generateAdapter = true)
 data class DataResult<Data>(
-    val success: Boolean,
+    @Json(name = "success") val isSuccessful: Boolean,
     val data: Data,
 )
 
 @JsonClass(generateAdapter = true)
 data class TopicIdResult(
-    val success: Boolean,
+    @Json(name = "success") val isSuccessful: Boolean,
     @Json(name = "topic_id") val topicId: String,
 )
 
 @JsonClass(generateAdapter = true)
 data class ReplyIdResult(
-    val success: Boolean,
+    @Json(name = "success") val isSuccessful: Boolean,
     @Json(name = "reply_id") val replyId: String,
 )
 
 @JsonClass(generateAdapter = true)
 data class UpReplyResult(
-    val success: Boolean,
+    @Json(name = "success") val isSuccessful: Boolean,
     val action: UpAction,
 )
 
 @JsonClass(generateAdapter = true)
 data class LoginResult(
-    val success: Boolean,
+    @Json(name = "success") val isSuccessful: Boolean,
     val id: String,
     @Json(name = "loginname") val loginName: String,
     @Json(name = "avatar_url") val avatarUrl: String,
@@ -42,12 +42,12 @@ data class LoginResult(
 
 @JsonClass(generateAdapter = true)
 data class MessagesMarkedAllResult(
-    val success: Boolean,
+    @Json(name = "success") val isSuccessful: Boolean,
     @Json(name = "marked_msgs") val markedMessages: List<IdData>,
 )
 
 @JsonClass(generateAdapter = true)
 data class MessageMarkedResult(
-    val success: Boolean,
+    @Json(name = "success") val isSuccessful: Boolean,
     @Json(name = "marked_msg_id") val markedMessageId: String,
 )
