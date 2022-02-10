b5bb40496b0e31ae4218d33716c773dcfc426a9f
TakWolf <takwolf@foxmail.com>
2022-02-10 11:39:23 +0800

    用户详情共享元素支持

--- app/src/main/java/org/cnodejs/android/md/ui/fragment/UserDetailFragment.kt
+++ app/src/main/java/org/cnodejs/android/md/ui/fragment/UserDetailFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.transition.TransitionInflater
 import org.cnodejs.android.md.R
 import org.cnodejs.android.md.databinding.FragmentUserDetailBinding
 import org.cnodejs.android.md.model.entity.IUser
@@ -17,19 +20,29 @@ class UserDetailFragment : BaseFragment() {
         private const val KEY_LOGIN_NAME = "login_name"
         private const val KEY_AVATAR_URL = "avatar_url"
 
-        fun open(fragment: Fragment, loginName: String?, avatarUrl: String? = null) {
-            if (loginName == null) {
+        fun open(fragment: Fragment, loginName: String) {
+            val args = Bundle()
+            args.putString(KEY_LOGIN_NAME, loginName)
+            NavUtils.push(fragment, R.id.fragment_user_detail, args, NavUtils.Anim.FADE)
+        }
+
+        fun open(fragment: Fragment, user: IUser, imgAvatar: ImageView) {
+            if (user.avatarUrl == null) {
                 return
             }
             val args = Bundle()
-            args.putString(KEY_LOGIN_NAME, loginName)
-            args.putString(KEY_AVATAR_URL, avatarUrl)
-            NavUtils.push(fragment, R.id.fragment_user_detail, args)
+            args.putString(KEY_LOGIN_NAME, user.loginName)
+            args.putString(KEY_AVATAR_URL, user.avatarUrl)
+            val extras = FragmentNavigatorExtras(imgAvatar to "img_avatar")
+            NavUtils.push(fragment, R.id.fragment_user_detail, args, extras = extras)
         }
+    }
 
-        fun open(fragment: Fragment, user: IUser) {
-            open(fragment, user.loginName, user.avatarUrl)
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val sharedElementTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+        sharedElementEnterTransition = sharedElementTransition
+        sharedElementReturnTransition = sharedElementTransition
     }
 
     override fun onCreateView(
