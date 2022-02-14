9dfe31d1d4e348e1adb465b7076a95efcf074fba
TakWolf <takwolf@foxmail.com>
2022-02-14 11:39:07 +0800

    用户详情页加载

--- app/src/main/java/org/cnodejs/android/md/ui/fragment/UserDetailFragment.kt
+++ app/src/main/java/org/cnodejs/android/md/ui/fragment/UserDetailFragment.kt
@@ -5,13 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.transition.TransitionInflater
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import org.cnodejs.android.md.R
 import org.cnodejs.android.md.databinding.FragmentUserDetailBinding
+import org.cnodejs.android.md.databinding.PageUserDetailTopicsBinding
 import org.cnodejs.android.md.model.entity.IUser
+import org.cnodejs.android.md.ui.adapter.TopicSimpleListAdapter
+import org.cnodejs.android.md.ui.listener.TopicDetailNavigateListener
+import org.cnodejs.android.md.ui.listener.UserDetailNavigateListener
 import org.cnodejs.android.md.util.Navigator
+import org.cnodejs.android.md.util.loadAvatar
 import org.cnodejs.android.md.vm.UserDetailViewModel
 
 class UserDetailFragment : BaseFragment() {
@@ -41,11 +49,18 @@ class UserDetailFragment : BaseFragment() {
 
     private val userDetailViewModel: UserDetailViewModel by viewModels()
 
+    private lateinit var loginName: String
+    private var avatarUrl: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
 
-        userDetailViewModel.loginName = requireArguments().getString(KEY_LOGIN_NAME)
+        val args = requireArguments()
+        loginName = args.getString(KEY_LOGIN_NAME)!!
+        avatarUrl = args.getString(KEY_AVATAR_URL)
+
+        userDetailViewModel.loginName = loginName
     }
 
     override fun onCreateView(
@@ -55,10 +70,81 @@ class UserDetailFragment : BaseFragment() {
     ): View {
         val binding = FragmentUserDetailBinding.inflate(inflater, container, false)
 
-        observeViewModel(userDetailViewModel)
+        binding.imgAuthor.loadAvatar(avatarUrl)
 
         // TODO
 
+        binding.viewPager.offscreenPageLimit = 3
+        binding.viewPager.adapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount(): Int {
+                return 3
+            }
+
+            override fun createFragment(position: Int): Fragment {
+                return TopicListFragment.create(loginName, position)
+            }
+        }
+
+        observeViewModel(userDetailViewModel)
+
         return binding.root
+    }
+
+    class TopicListFragment : BaseFragment() {
+        companion object {
+            const val TYPE_REPLY = 0
+            const val TYPE_CREATE = 1
+            const val TYPE_COLLECT = 2
+
+            private const val KEY_TYPE = "type"
+
+            fun create(loginName: String, type: Int): TopicListFragment {
+                val fragment = TopicListFragment()
+                fragment.arguments = Bundle().apply {
+                    putString(KEY_LOGIN_NAME, loginName)
+                    putInt(KEY_TYPE, type)
+                }
+                return fragment
+            }
+        }
+
+        private val userDetailViewModel: UserDetailViewModel by viewModels({ requireParentFragment() })
+
+        private lateinit var loginName: String
+        private var type: Int  = -1
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+            val args = requireArguments()
+            loginName = args.getString(KEY_LOGIN_NAME)!!
+            type = args.getInt(KEY_TYPE, -1)
+        }
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?,
+        ): View {
+            val binding = PageUserDetailTopicsBinding.inflate(inflater, container, false)
+
+            binding.recyclerView.layoutManager = LinearLayoutManager(context)
+            val adapter = TopicSimpleListAdapter()
+            adapter.onTopicClickListener = TopicDetailNavigateListener(navigator)
+            adapter.onUserClickListener = UserDetailNavigateListener(navigator, loginName)
+            binding.recyclerView.adapter = adapter
+
+            userDetailViewModel.userDetailData.observe(viewLifecycleOwner) {
+                it?.let { userDetail ->
+                    when (type) {
+                        TYPE_REPLY -> adapter.submitList(userDetail.user.recentReplies.toList())
+                        TYPE_CREATE -> adapter.submitList(userDetail.user.recentTopics.toList())
+                        TYPE_COLLECT -> adapter.submitList(userDetail.collectTopics.toList())
+                    }
+                }
+            }
+
+            return binding.root
+        }
     }
 }
