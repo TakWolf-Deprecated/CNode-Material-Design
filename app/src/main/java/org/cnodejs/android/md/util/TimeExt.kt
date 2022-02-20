1b635b2dd531e9f188a37aec5eaa575358099f1c
TakWolf <takwolf@foxmail.com>
2022-02-20 18:05:21 +0800

    扩展时间工具

--- app/src/main/java/org/cnodejs/android/md/util/TimeExt.kt
+++ app/src/main/java/org/cnodejs/android/md/util/TimeExt.kt
@@ -10,14 +10,22 @@ private const val MINUTE = 60 * 1000L
 private const val HOUR = 60 * MINUTE
 private const val DAY = 24 * HOUR
 private const val WEEK = 7 * DAY
+private const val MONTH = 31 * DAY
+private const val YEAR = 12 * MONTH
 
 private val displayDateTimeFormatter = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm")
 
 fun OffsetDateTime.timeSpanStringFromNow(resources: Resources): String {
     val offset = Duration.between(this, OffsetDateTime.now()).toMillis()
     return when {
-        offset > WEEK -> {
+        offset > YEAR -> {
             format(displayDateTimeFormatter)
+        }
+        offset > MONTH -> {
+            resources.getString(R.string.d_months_ago, offset / MONTH)
+        }
+        offset > WEEK -> {
+            resources.getString(R.string.d_weeks_ago, offset / WEEK)
         }
         offset > DAY -> {
             resources.getString(R.string.d_days_ago, offset / DAY)
