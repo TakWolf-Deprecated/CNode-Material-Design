c2e129d3e82afa2856dcce0b65907c86036c4046
TakWolf <takwolf@foxmail.com>
2022-02-23 00:20:12 +0800

    话题详情

--- app/src/main/java/org/cnodejs/android/md/vm/TopicDetailViewModel.kt
+++ app/src/main/java/org/cnodejs/android/md/vm/TopicDetailViewModel.kt
@@ -2,7 +2,18 @@ package org.cnodejs.android.md.vm
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import org.cnodejs.android.md.model.api.CNodeClient
+import org.cnodejs.android.md.model.store.AppStoreHolder
+import org.cnodejs.android.md.vm.holder.IToastViewModel
+import org.cnodejs.android.md.vm.holder.ToastLiveHolder
 
-class TopicDetailViewModel(application: Application) : AndroidViewModel(application) {
-    // TODO
+class TopicDetailViewModel(application: Application) : AndroidViewModel(application), IToastViewModel {
+    companion object {
+        private const val TAG = "TopicDetailViewModel"
+    }
+
+    private val accountStore = AppStoreHolder.getInstance(application).accountStore
+    private val api = CNodeClient.getInstance(application).api
+
+    override val toastHolder = ToastLiveHolder()
 }
