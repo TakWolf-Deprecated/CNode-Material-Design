4c37ee725b672a28fa1de6bee11644ed1c9fa230
TakWolf <takwolf@foxmail.com>
2022-02-23 17:14:43 +0800

    话题详情框架

---
+++ app/src/main/java/org/cnodejs/android/md/ui/adapter/ReplyListAdapter.kt
@@ -0,0 +1,60 @@
+package org.cnodejs.android.md.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import org.cnodejs.android.md.databinding.ItemReplyBinding
+import org.cnodejs.android.md.model.entity.Reply
+import org.cnodejs.android.md.ui.listener.OnUserClickListener
+
+class ReplyListAdapter(private val layoutInflater: LayoutInflater, private val who: String) : ListAdapter<Reply, ReplyListAdapter.ViewHolder>(ReplyDiffItemCallback) {
+    var myId: String? = null
+    set(value) {
+        if (field != value) {
+            field = value
+            notifyItemRangeChanged(0, itemCount)
+        }
+    }
+
+    var onBtnUpClickListener: ((reply: Reply) -> Unit)? = null
+    var onBtnAtClickListener: ((reply: Reply) -> Unit)? = null
+    var onUserClickListener: OnUserClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemReplyBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(binding, who)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position), myId, position == itemCount - 1)
+    }
+
+    class ViewHolder(
+        private val binding: ItemReplyBinding,
+        private val who: String,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            // TODO
+        }
+
+        fun bind(reply: Reply, myId: String?, isLast: Boolean) {
+            // TODO
+
+            binding.tvContent.text = reply.content // TODO
+            binding.divider.isVisible = !isLast
+        }
+    }
+}
+
+private object ReplyDiffItemCallback : DiffUtil.ItemCallback<Reply>() {
+    override fun areItemsTheSame(oldItem: Reply, newItem: Reply): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Reply, newItem: Reply): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/org/cnodejs/android/md/ui/fragment/TopicDetailFragment.kt
+++ app/src/main/java/org/cnodejs/android/md/ui/fragment/TopicDetailFragment.kt
@@ -1,15 +1,25 @@
 package org.cnodejs.android.md.ui.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ColorInt
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.fragment.app.who
+import androidx.recyclerview.widget.LinearLayoutManager
 import org.cnodejs.android.md.R
 import org.cnodejs.android.md.databinding.FragmentTopicDetailBinding
 import org.cnodejs.android.md.model.entity.ITopic
+import org.cnodejs.android.md.ui.adapter.ReplyListAdapter
+import org.cnodejs.android.md.ui.listener.OnDoubleClickListener
+import org.cnodejs.android.md.ui.listener.UserDetailNavigateListener
+import org.cnodejs.android.md.ui.widget.TopicDetailHeader
 import org.cnodejs.android.md.util.NavAnim
 import org.cnodejs.android.md.util.Navigator
+import org.cnodejs.android.md.vm.AccountViewModel
 import org.cnodejs.android.md.vm.TopicDetailViewModel
 
 class TopicDetailFragment : BaseFragment() {
@@ -30,7 +40,26 @@ class TopicDetailFragment : BaseFragment() {
         }
     }
 
+    private val accountViewModel: AccountViewModel by activityViewModels()
     private val topicDetailViewModel: TopicDetailViewModel by viewModels()
+
+    private lateinit var topicId: String
+    private var jumpToReplyId: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val args = requireArguments()
+        topicId = args.getString(KEY_TOPIC_ID)!!
+        jumpToReplyId = (savedInstanceState ?: args).getString(KEY_JUMP_TO_REPLY_ID)
+
+        topicDetailViewModel.topicId = topicId
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_JUMP_TO_REPLY_ID, jumpToReplyId)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,11 +68,81 @@ class TopicDetailFragment : BaseFragment() {
     ): View {
         val binding = FragmentTopicDetailBinding.inflate(inflater, container, false)
 
-        // TODO
+        val a = requireContext().obtainStyledAttributes(intArrayOf(android.R.attr.colorAccent))
+        @ColorInt val colorAccent = a.getColor(0, Color.TRANSPARENT)
+        a.recycle()
+
+        binding.toolbar.setNavigationOnClickListener {
+            navigator.back()
+        }
+        binding.toolbar.setOnClickListener(object : OnDoubleClickListener() {
+            override fun onDoubleClick(v: View) {
+                binding.recyclerView.scrollToPosition(0)
+            }
+        })
+        binding.toolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.btn_share -> {
+                    // TODO
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.refreshLayout.setColorSchemeColors(colorAccent)
+        binding.refreshLayout.setOnRefreshListener {
+            topicDetailViewModel.loadTopicDetail()
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        val header = TopicDetailHeader(inflater, binding.recyclerView, topicDetailViewModel).apply {
+            myId = accountViewModel.accountData.value?.id
+        }
+        val adapter = ReplyListAdapter(inflater, who).apply {
+            myId = accountViewModel.accountData.value?.id
+            onBtnUpClickListener = { reply ->
+                // TODO
+            }
+            onBtnAtClickListener = { reply ->
+                // TODO
+            }
+            onUserClickListener = UserDetailNavigateListener(navigator)
+        }
+        binding.recyclerView.adapter = adapter
 
         observeViewModel(topicDetailViewModel)
 
-        // TODO
+        accountViewModel.accountData.observe(viewLifecycleOwner) { account ->
+            account?.id.apply {
+                header.myId = this
+                adapter.myId = this
+            }
+        }
+
+        topicDetailViewModel.loadingStateData.observe(viewLifecycleOwner) {
+            it?.let { isLoading ->
+                binding.refreshLayout.isRefreshing = isLoading
+            }
+        }
+
+        topicDetailViewModel.topicDetailData.observe(viewLifecycleOwner) {
+            it?.let { topicDetail ->
+                header.updateViews(topicDetail)
+                adapter.submitList(topicDetail.replies.toList())
+                jumpToReplyId?.let { jumpToReplyId ->
+                    this.jumpToReplyId = null
+                    for (i in topicDetail.replies.indices) {
+                        if (topicDetail.replies[i].id == jumpToReplyId) {
+                            val layoutPosition = i + 1 // TODO
+                            binding.recyclerView.scrollToPosition(layoutPosition)
+                            break
+                        }
+                    }
+                }
+            }
+        }
+
+        topicDetailViewModel.onViewStart()
 
         return binding.root
     }
