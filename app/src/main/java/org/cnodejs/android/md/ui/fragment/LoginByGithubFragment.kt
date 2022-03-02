1f28be73d4c560da8ec186863b293d9ae5a2cd3c
TakWolf <takwolf@foxmail.com>
2022-03-02 17:36:37 +0800

    调整 WebView 状态保存

--- app/src/main/java/org/cnodejs/android/md/ui/fragment/LoginByGithubFragment.kt
+++ app/src/main/java/org/cnodejs/android/md/ui/fragment/LoginByGithubFragment.kt
@@ -1,36 +1,31 @@
 package org.cnodejs.android.md.ui.fragment
 
-import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.*
+import android.webkit.CookieManager
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
-import org.cnodejs.android.md.BuildConfig
 import org.cnodejs.android.md.R
 import org.cnodejs.android.md.databinding.FragmentLoginByGithubBinding
 import org.cnodejs.android.md.model.api.CNodeDefine
-import org.cnodejs.android.md.util.JsonUtils
 import org.cnodejs.android.md.util.Navigator
 import org.cnodejs.android.md.vm.LoginByGithubViewModel
 
 class LoginByGithubFragment : BaseFragment() {
     companion object {
-        private const val TAG = "LoginByGithubFragment"
-
         const val REQUEST_KEY = "requestKey.LoginByGithub"
 
         const val EXTRA_ACCESS_TOKEN = "accessToken"
-        private const val EXTRA_CURRENT_URL = "currentUrl"
-        private const val EXTRA_CURRENT_HTML = "currentHtml"
 
         fun open(navigator: Navigator) {
             navigator.push(R.id.fragment_login_by_github)
@@ -39,68 +34,33 @@ class LoginByGithubFragment : BaseFragment() {
 
     private val loginByGithubViewModel: LoginByGithubViewModel by viewModels()
 
-    private var _binding: FragmentLoginByGithubBinding? = null
-
     private val cookieManager = CookieManager.getInstance()
-
-    private lateinit var currentUrl: String
-    private var currentHtml: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        savedInstanceState?.let { state ->
-            currentUrl = state.getString(EXTRA_CURRENT_URL)!!
-            currentHtml = state.getString(EXTRA_CURRENT_HTML)
-        } ?: run {
-            currentUrl = CNodeDefine.GITHUB_LOGIN_URL
+        if (savedInstanceState == null) {
             cookieManager.removeAllCookies {}
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        _binding?.let { binding ->
-            outState.putString(EXTRA_CURRENT_URL, currentUrl)
-            outState.putString(EXTRA_CURRENT_HTML, currentHtml)
-        }
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         val binding = FragmentLoginByGithubBinding.inflate(inflater, container, false)
-        _binding = binding
 
         binding.toolbar.setNavigationOnClickListener {
             navigator.back()
         }
 
-        binding.web.settings.javaScriptEnabled = true
         binding.web.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-                if (BuildConfig.DEBUG) {
-                    Log.d(TAG, "--> ${request.url}")
-                }
-                return super.shouldOverrideUrlLoading(view, request)
-            }
-
             override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
-                currentUrl = url
-                currentHtml = null
                 if (url == CNodeDefine.HOST_BASE_URL || url == "${CNodeDefine.HOST_BASE_URL}/") {
                     cookieManager.getCookie(url)?.let { cookie ->
                         loginByGithubViewModel.fetchAccessToken(cookie)
                     }
-                }
-            }
-
-            override fun onPageFinished(view: WebView, url: String) {
-                view.evaluateJavascript("(function() { return '<html>' + document.getElementsByTagName('html')[0].innerHTML + '</html>'; })();") { json ->
-                    currentHtml = JsonUtils.moshi.adapter(String::class.java).fromJson(json)
                 }
             }
         }
@@ -110,10 +70,8 @@ class LoginByGithubFragment : BaseFragment() {
                 binding.progressBar.isVisible = newProgress < 100
             }
         }
-        currentHtml?.let { html ->
-            binding.web.loadDataWithBaseURL(currentUrl, html, "text/html", "utf-8", null)
-        } ?: run {
-            binding.web.loadUrl(currentUrl)
+        if (savedInstanceState == null) {
+            binding.web.loadUrl(CNodeDefine.GITHUB_LOGIN_URL)
         }
 
         viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
@@ -137,10 +95,5 @@ class LoginByGithubFragment : BaseFragment() {
         }
 
         return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
---
+++ app/src/main/java/org/cnodejs/android/md/ui/widget/StableWebView.java
@@ -0,0 +1,116 @@
+package org.cnodejs.android.md.ui.widget;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+import androidx.annotation.AttrRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.annotation.StyleRes;
+
+public class StableWebView extends WebView {
+    public StableWebView(@NonNull Context context) {
+        super(context);
+        init(context);
+    }
+
+    public StableWebView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public StableWebView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public StableWebView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr, @StyleRes int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(@NonNull Context context) {
+        // TODO
+    }
+
+    @Nullable
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        SavedState savedState = new SavedState(super.onSaveInstanceState());
+        saveState(savedState.webState);
+        savedState.url = getUrl();
+        return savedState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (!(state instanceof SavedState)) {
+            super.onRestoreInstanceState(state);
+            return;
+        }
+        SavedState savedState = (SavedState) state;
+        restoreState(savedState.webState);
+        loadUrl(savedState.url);
+        super.onRestoreInstanceState(savedState.getSuperState());
+    }
+
+    private static class SavedState extends BaseSavedState {
+        Bundle webState;
+        String url;
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+            webState = new Bundle();
+        }
+
+        public SavedState(Parcel source) {
+            super(source);
+            readValues(source, null);
+        }
+
+        @RequiresApi(Build.VERSION_CODES.N)
+        public SavedState(Parcel source, ClassLoader loader) {
+            super(source, loader);
+            readValues(source, loader);
+        }
+
+        private void readValues(@NonNull Parcel source, @Nullable ClassLoader loader) {
+            webState = source.readBundle(loader);
+            url = source.readString();
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeBundle(webState);
+            out.writeString(url);
+        }
+
+        public static final Creator<SavedState> CREATOR = new ClassLoaderCreator<SavedState>() {
+            @Override
+            public SavedState createFromParcel(Parcel source, ClassLoader loader) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    return new SavedState(source, loader);
+                } else {
+                    return new SavedState(source);
+                }
+            }
+
+            @Override
+            public SavedState createFromParcel(Parcel source) {
+                return createFromParcel(source, null);
+            }
+
+            @Override
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+    }
+}
