40c83b373e5365a08faca2b5816e44ab03cc9bef
TakWolf <takwolf@foxmail.com>
2022-07-11 16:42:37 +0800

    升级构建

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 32
 
     defaultConfig {
         applicationId = "org.cnodejs.android.md"
         minSdk = 21
-        targetSdk = 31
+        targetSdk = 32
         versionCode = 1
         versionName = "2.0.0"
 
@@ -52,24 +52,24 @@ android {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.3")
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("androidx.activity:activity-ktx:1.4.0")
     implementation("androidx.fragment:fragment-ktx:1.4.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
     implementation("androidx.datastore:datastore:1.0.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.camera:camera-camera2:1.1.0-beta01")
-    implementation("androidx.camera:camera-lifecycle:1.1.0-beta01")
-    implementation("androidx.camera:camera-view:1.1.0-beta01")
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.camera:camera-camera2:1.1.0-rc02")
+    implementation("androidx.camera:camera-lifecycle:1.1.0-rc02")
+    implementation("androidx.camera:camera-view:1.1.0-rc02")
+    implementation("com.google.android.material:material:1.6.1")
     implementation("com.squareup.moshi:moshi:1.13.0")
     ksp("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.9.3"))
@@ -77,20 +77,20 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("io.coil-kt:coil-base:1.4.0")
-    implementation("io.coil-kt:coil-gif:1.4.0")
-    implementation("io.coil-kt:coil-svg:1.4.0")
+    implementation("io.coil-kt:coil-base:2.1.0")
+    implementation("io.coil-kt:coil-gif:2.1.0")
+    implementation("io.coil-kt:coil-svg:2.1.0")
     implementation("org.greenrobot:eventbus:3.3.1")
-    implementation("org.jsoup:jsoup:1.14.3")
-    implementation("org.commonmark:commonmark:0.18.2")
-    implementation("org.commonmark:commonmark-ext-autolink:0.18.2")
-    implementation("org.commonmark:commonmark-ext-gfm-strikethrough:0.18.2")
-    implementation("org.commonmark:commonmark-ext-gfm-tables:0.18.2")
-    implementation("org.commonmark:commonmark-ext-heading-anchor:0.18.2")
-    implementation("org.commonmark:commonmark-ext-ins:0.18.2")
-    implementation("org.commonmark:commonmark-ext-yaml-front-matter:0.18.2")
-    implementation("org.commonmark:commonmark-ext-image-attributes:0.18.2")
-    implementation("org.commonmark:commonmark-ext-task-list-items:0.18.2")
+    implementation("org.jsoup:jsoup:1.15.2")
+    implementation("org.commonmark:commonmark:0.19.0")
+    implementation("org.commonmark:commonmark-ext-autolink:0.19.0")
+    implementation("org.commonmark:commonmark-ext-gfm-strikethrough:0.19.0")
+    implementation("org.commonmark:commonmark-ext-gfm-tables:0.19.0")
+    implementation("org.commonmark:commonmark-ext-heading-anchor:0.19.0")
+    implementation("org.commonmark:commonmark-ext-ins:0.19.0")
+    implementation("org.commonmark:commonmark-ext-yaml-front-matter:0.19.0")
+    implementation("org.commonmark:commonmark-ext-image-attributes:0.19.0")
+    implementation("org.commonmark:commonmark-ext-task-list-items:0.19.0")
     implementation("com.github.TakWolf.Android-HeaderAndFooterRecyclerView:hfrecyclerview:0.0.3")
     implementation("com.github.TakWolf.Android-HeaderAndFooterRecyclerView:loadmorefooter:0.0.3")
     implementation("com.github.TakWolf.Android-InsetsWidget:insetswidget:0.0.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") version "7.1.2" apply false
-    id("com.android.library") version "7.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
-    id("com.google.devtools.ksp") version "1.6.10-1.0.2" apply false
+    id("com.android.application") version "7.2.1" apply false
+    id("com.android.library") version "7.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    id("com.google.devtools.ksp") version "1.7.10-1.0.6" apply false
 }
 
 tasks {
